ffa809667f6f4eea24e4229a018de1c573ab7637
code-vibe <mrsamoladapo@gmail.com>
2022-09-26 16:06:25 +0100

    Array: Created Array class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 10;
-        numbers[1] = 20;
-        numbers[2] = 30;
-        numbers[3] = 40;
-        System.out.println(Arrays.toString(numbers));
     }
-}
+    }
