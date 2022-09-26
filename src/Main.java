bde2430951833bb9a019ee737a473dcf11946408
code-vibe <mrsamoladapo@gmail.com>
2022-09-26 09:13:08 +0100

    Main: Increased the array Index

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[4];
+        int[] numbers = new int[5];
         System.out.println(Arrays.toString(numbers));
     }
 }
