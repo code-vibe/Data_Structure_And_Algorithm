3d882a9687a973592dba4f4138b2e69f47176f4b
code-vibe <mrsamoladapo@gmail.com>
2022-09-26 19:34:01 +0100

    Array: Created Array class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.remove(0);
+
+        System.out.println(list.indexOf(20));
     }
     }
