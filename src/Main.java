01b68c034abbc63058b505547418f543b26e4e75
code-vibe <mrsamoladapo@gmail.com>
2022-09-26 09:40:39 +0100

    Updated array index

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] numbers = new int[5];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
         System.out.println(Arrays.toString(numbers));
     }
 }
