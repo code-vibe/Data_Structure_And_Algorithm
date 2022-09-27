0c77d6849b61773eedd5eef8664dbe3e180c4a6d
code-vibe <mrsamoladapo@gmail.com>
2022-09-27 08:24:26 +0100

    LinkedList: Created a linked lis-for DSA

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        System.out.println(list);
         list.add(10);
-        System.out.println(list);
         list.add(20);
         list.add(30);
         list.add(40);
+        list.addFirst(500);
+        list.add(3,30);
         System.out.println(list);
     }
     }
