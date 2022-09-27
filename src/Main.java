d2ed7da38f97e0f6dfc04e45e8a9f6d3c0ae452f
code-vibe <mrsamoladapo@gmail.com>
2022-09-27 08:18:13 +0100

    LinkedList: Created a linked lis-for DSA

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
+        LinkedList list = new LinkedList();
+        System.out.println(list);
         list.add(10);
+        System.out.println(list);
         list.add(20);
         list.add(30);
         list.add(40);
-        list.remove(0);
-
-        System.out.println(list.indexOf(20));
+        System.out.println(list);
     }
     }
