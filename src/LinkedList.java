dd2af463a27ab99080f404d5073b84d0e4f2e867
code-vibe <mrsamoladapo@gmail.com>
2022-09-27 09:56:33 +0100

    LinkedList: Update a linked lis-for DSA

---
+++ src/LinkedList.java
@@ -0,0 +1,23 @@
+public class LinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+    private Node first;
+    private Node last;
+
+    // addFirst
+    // addLast
+    public void addLast(int item) {
+        var node = new Node();
+
+    }
+    // deleteFirst
+    // deleteLast
+    // containsLast
+    // indexOf
+}
