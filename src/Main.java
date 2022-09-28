f302482b74c86e352a370a1ddfad24d0a5aaa129
code-vibe <mrsamoladapo@gmail.com>
2022-09-28 07:57:34 +0100

    LinkedList: Update a linked lis-for DSA

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
       var list = new LinkedList();
-      list.addLast(12);
-      list.addLast(15);
-      list.addLast(25);
+//      list.addLast(12);
+//      list.addLast(15);
+//      list.addLast(25);
+        list.removeFirst();
       System.out.println(list.contains(12));
     }
     }
